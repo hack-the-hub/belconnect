4b2830384f9cfd05f7dee6cb2e37aac43a42c423
sofia.tzima <sofia.tzima@workday.com>
2016-09-10 15:41:17 +0100

    removed created field in belstops

--- api/models.py
+++ api/models.py
@@ -6,16 +6,12 @@ from rest_framework import viewsets
 
 # Create your models here.
 class BelStop(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
     location = models.CharField(max_length=200)
     spots_available = models.IntegerField(default=0)
     # days_available = SeparatedValuesField()
     times_available = models.CommaSeparatedIntegerField(max_length=200, default=0)
     accepts_belPoints = models.BooleanField(default=True)
     owner = models.ForeignKey('auth.User', related_name='belstops')
-
-    class Meta:
-        ordering = ('created',)
 
 class BelMeeting(models.Model):
     belStop = models.ForeignKey(BelStop)
