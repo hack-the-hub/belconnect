972ddf07e8a24850aa69b721df14bd060b3ed0ed
Elizabet Oliveira <elizabet.oliveira@gmail.com>
2016-09-10 11:50:39 +0100

    wep-app folder and login

--- web-app/src/app/index.route.js
+++ web-app/src/app/index.route.js
@@ -13,6 +13,12 @@
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
+      })
+      .state('dashboard', {
+        url: '/dashboard',
+        templateUrl: 'app/dashboard/dashboard.html',
+        controller: 'MainController',
+        controllerAs: 'main'
       });
 
     $urlRouterProvider.otherwise('/');
