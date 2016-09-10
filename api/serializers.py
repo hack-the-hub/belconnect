6a735f3efef7ba0f31789245e01bd5eec8ee8fae
LauraU <laura.uzcategui@workday.com>
2016-09-10 14:03:42 +0100

    Update to serialisers

--- api/serializers.py
+++ api/serializers.py
@@ -2,14 +2,6 @@ from rest_framework import serializers
 from api.models import BelStop
 from django.contrib.auth.models import User
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    bel_stops = serializers.HyperlinkedRelatedField(many=True, view_name='belstop-detail', read_only=True)
-    #bel_meetings  = serializers.HyperlinkedRelatedField(many=True, view_name='belmeeting-detail', read_only=True)
-
-    class Meta:
-        model = User
-        fields = ('url', 'pk', 'username', 'bel_stops')
-
 class BelStopSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
 
@@ -17,3 +9,11 @@ class BelStopSerializer(serializers.HyperlinkedModelSerializer):
         model = BelStop
         fields = ('url', 'pk', 'owner', 'location',
             'spots_available','times_available', 'accepts_belPoints')
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    belstops = serializers.HyperlinkedRelatedField(many=True, view_name='belstop-detail', read_only=True)
+    #bel_meetings  = serializers.HyperlinkedRelatedField(many=True, view_name='belmeeting-detail', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ('url', 'pk', 'username', 'belstops')
