923ba6d0ec4789c0ddaaa9026af30e0b2e4e08ee
Elizabet Oliveira <elizabet.oliveira@gmail.com>
2016-09-10 10:30:46 +0100

    Angular project

---
+++ assets/src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('belconnect')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
